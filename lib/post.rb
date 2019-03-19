764a23a02085cc418c941e23d5b77d3794760369
Henry Sztanski <henry@sztanski.com>
2019-03-19 00:24:36 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,14 @@
 class Post
   attr_accessor :title, :author
-  attr_reader :all
 
   @@all = []
 
   def initialize(title)
     @title = title
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
