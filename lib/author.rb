cc7c044f22ff646ca73ba5597db89d046c4d2e30
Henry Sztanski <henry@sztanski.com>
2019-03-19 00:18:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,14 +14,14 @@ class Author
     post.author = self
   end
 
-  # def add_post_by_title(title)
-  #   post = Post.new(title)
-  #   add_post(post)
-  # end
-  #
-  # def self.post_count
-  #   post_count = 0
-  #   @@authors.each {|author| post_count += author.posts.count}
-  #   post_count
-  # end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+  
+  def self.post_count
+    post_count = 0
+    @@authors.each {|author| post_count += author.posts.count}
+    post_count
+  end
 end
